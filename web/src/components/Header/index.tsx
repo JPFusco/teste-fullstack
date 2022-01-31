d0745d1eb3b4aa41557018dfc9de8759ec697525
JPFusco <85804722+JPFusco@users.noreply.github.com>
2022-01-31 00:14:30 -0300

    Adicionando signout no header

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,18 +1,27 @@
 import './style.css';
 import Logo from '../../assets/logo.svg';
+import IconSignOut from '../../assets/sign-out.svg';
+import useGlobalContext from '../../hooks/useGlobalContext';
 
 export default function Header() {
+  const { removeAuthToken } = useGlobalContext();
+
+  const handleSignOut = () => {
+    removeAuthToken();
+  }
+
   return (
     <div className="header-container">
       <header>
         <div className="header-left">
-          <img src={Logo} alt="Gota d'água escrito fullstack ao lado" />
+          <img src={Logo} alt="Logo fullstack" />
         </div>
         <div className="header-right">
           <input
             type="text"
             placeholder='BUSCA por um veículo'
           />
+          <img src={IconSignOut} alt="Sign Out" onClick={handleSignOut} />
         </div>
       </header>
     </div>

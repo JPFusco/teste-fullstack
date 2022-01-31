1c8418f66386457bbc54e099dc4638e48fec480b
JPFusco <85804722+JPFusco@users.noreply.github.com>
2022-01-30 21:32:11 -0300

    Página SignUp (html e css)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import GlobalContextProvider from './contexts/GlobalContext';
 
 function App() {
   return (
     <div className="App">
       <GlobalContextProvider>
-        <SignIn />
+        <SignUp />
       </GlobalContextProvider>
     </div>
   );
--- web/src/pages/SignUp/index.tsx
+++ web/src/pages/SignUp/index.tsx
@@ -1,9 +1,54 @@
-import 'style.css';
+import Logo from '../../assets/logo.svg';
+import './style.css';
+import TextField from '@mui/material/TextField';
 
 export default function SignUp() {
   return (
     <div className="sign-up-container">
-
+      <div className="sign-up-left">
+        <img src={Logo} alt="Logo da Fullstack" />
+      </div>
+      <div className="sign-up-right">
+        <h1>
+          Sign In
+        </h1>
+        <form>
+          <TextField
+            id="standard-basic"
+            name="email"
+            label="E-mail"
+            variant="standard"
+            sx={{ marginBottom: '15px' }}
+          // value={}
+          // onChange={}
+          />
+          <TextField
+            id="standard-basic"
+            name="senha"
+            label="Senha"
+            variant="standard"
+            sx={{ marginBottom: '15px' }}
+          // value={}
+          // onChange={}
+          />
+          <TextField
+            id="standard-basic"
+            name="confirmar-senha"
+            label="Confirmar Senha"
+            variant="standard"
+            sx={{ marginBottom: '35px' }}
+          // value={}
+          // onChange={}
+          />
+          <button>
+            CADASTRAR
+          </button>
+        </form>
+        <h2>
+          Possui uma conta?
+          {/*  <Link to='/sign-up'>Entrar</Link> */}
+        </h2>
+      </div>
     </div>
   );
 }
